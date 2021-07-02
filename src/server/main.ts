03d9cf5c37b3d900db1c2e310efdf0d947b6884b
Brian Kopp <briankopp.usa@gmail.com>
2021-07-01 21:53:07 -0600

    Bootstrap the app middleware

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import * as session from 'express-session';
 import { AppModule } from './app.module';
+import * as passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    session({
+      secret: 'foobar',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
+  // init 'passport' (npm install passport)
+  app.use(passport.initialize());
+  app.use(passport.session());
   await app.listen(3000);
 }
 bootstrap();
