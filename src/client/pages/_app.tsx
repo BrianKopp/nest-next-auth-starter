7f4a97350b41bcc29c5e011020da720446088a77
Brian Kopp <briankopp.usa@gmail.com>
2021-07-01 19:39:54 -0600

    Add some server side generation examples from mui-org/material-ui/examples/nextjs

---
+++ src/client/pages/_app.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Head from 'next/head';
+
+const MyApp = (props) => {
+  const { Component, pageProps } = props;
+  React.useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>My page</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </React.Fragment>
+  );
+};
+
+export default MyApp;
