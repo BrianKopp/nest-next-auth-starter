8c982122abc3c3d0ff67fa80ad2ab780d924f9d1
Brian Kopp <briankopp.usa@gmail.com>
2021-07-01 21:54:53 -0600

    Fix the HTTP config for logging in

--- src/server/modules/auth/controllers/auth.controller.ts
+++ src/server/modules/auth/controllers/auth.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Controller, HttpCode, Post, Req, UseGuards } from '@nestjs/common';
 import { Request } from 'express';
 import { LocalAuthGuard } from '../guards/local.guard';
 
 @Controller('api/v1/auth')
 export class AuthController {
   @UseGuards(LocalAuthGuard)
-  @Post('auth/login')
+  @Post('login')
+  @HttpCode(200)
   async login(@Req() req: Request) {
     return req.user;
   }
