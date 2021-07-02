00152d6fbb88ae99904f84cf25101ab3f41a77b7
Brian Kopp <briankopp.usa@gmail.com>
2021-07-01 19:00:50 -0600

    Try doing more things

--- src/client/pages/index.tsx
+++ src/client/pages/index.tsx
@@ -1,8 +1,33 @@
-import React from 'react';
-import { NextPage } from 'next';
+import React, { FunctionComponent } from 'react';
 
-const Home: NextPage = () => {
-  return <h1>Hello, World!</h1>;
+interface Props {
+  user: any;
+}
+
+export const getServerSideProps = async function ({ req, res }) {
+  const user = req.session.get('user');
+  console.log('checking the user session', user);
+  if (!user) {
+    console.log('user session not found, zoinks!');
+    return {
+      redirect: {
+        destination: '/public',
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: { user },
+  };
+};
+
+const Home: FunctionComponent<Props> = ({ user }) => {
+  return (
+    <div>
+      <h1>Welcome to your profile!</h1>
+      <pre>{JSON.stringify(user)}</pre>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/client/pages/public.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent } from 'react';
+
+const Public: FunctionComponent = () => {
+  return <h1>You're not logged in.</h1>;
+};
+
+export default Public;
