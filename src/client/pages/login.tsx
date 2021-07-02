96ebf999dda0bb7129503abcfc6d5e0c679afc18
Brian Kopp <briankopp.usa@gmail.com>
2021-07-01 20:40:36 -0600

    Add more to the login page

--- src/client/pages/login.tsx
+++ src/client/pages/login.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import {
+  Button,
   Card,
+  CardActions,
   CardContent,
-  Container,
+  CardHeader,
   makeStyles,
+  TextField,
   Theme,
 } from '@material-ui/core';
 import { getLoggedOutUserServerSideProps } from '../shared/utils/get-logged-in-user';
@@ -14,7 +17,10 @@ export const getServerSideProps = async function (context) {
 };
 
 const useStyles = makeStyles((theme: Theme) => ({
-  someClass: {},
+  card: {
+    paddingTop: 0,
+    maxWidth: '400px',
+  },
 }));
 
 const Login = (): JSX.Element => {
@@ -23,10 +29,51 @@ const Login = (): JSX.Element => {
     <>
       <NavBar />
       <div className="flex flexcolumn aligncenter mt16">
-        <Card raised>
-          <CardContent>
-            <p>Do something</p>
+        <Card raised className="p8">
+          <CardHeader title="Login"></CardHeader>
+          <CardContent className={classes.card}>
+            <form noValidate>
+              <div>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  name="email"
+                  label="Email Address"
+                  placeholder="someone@example.com"
+                  autoComplete="email"
+                  autoFocus
+                />
+              </div>
+              <div className="mt8">
+                <TextField
+                  required
+                  fullWidth
+                  className="mt16"
+                  id="password"
+                  name="password"
+                  label="Password"
+                  type="password"
+                  autoComplete="current-password"
+                />
+              </div>
+            </form>
           </CardContent>
+          <CardActions className="jc">
+            <div className="flex flexcolumn">
+              <div className="flex jc">
+                <Button variant="contained" color="primary">
+                  Login
+                </Button>
+              </div>
+              <div className="mt16 flex jc">
+                <a href="/register">Register new account</a>
+              </div>
+              <div className="mt8 flex jc">
+                <a href="/password-reset">Forgot password?</a>
+              </div>
+            </div>
+          </CardActions>
         </Card>
       </div>
     </>
