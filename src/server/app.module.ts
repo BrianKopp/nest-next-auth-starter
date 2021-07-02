ff3d2d2235a0082c2df2c58d4ccdf618744da85f
Brian Kopp <briankopp.usa@gmail.com>
2021-07-01 19:12:49 -0600

    Remove a bunch of stuff that we don't need

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ViewsModule } from './modules/views/views.module';
 
 @Module({
   imports: [ViewsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
