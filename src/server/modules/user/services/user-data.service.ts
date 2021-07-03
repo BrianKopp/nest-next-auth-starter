5a92b6c6f6b7730ba126f6d1e623621c503d89ac
Brian Kopp <briankopp.usa@gmail.com>
2021-07-02 19:55:04 -0600

    Make user-data service a bit smarter

--- src/server/modules/user/services/user-data.service.ts
+++ src/server/modules/user/services/user-data.service.ts
@@ -12,33 +12,55 @@ export class UserDataService {
     private oldPasswords: Repository<UserPasswordHistory>,
   ) {}
 
-  async getUser(criteria: {
-    username?: string;
-    email?: string;
-  }): Promise<User> {
-    if (!criteria || (!criteria.username && !criteria.email)) {
-      console.error('error getting user, no criteria', criteria);
-      throw new Error('cannot look up user with empty criteria');
+  async getUser(lookup: string): Promise<User> {
+    if (!lookup || typeof lookup !== 'string') {
+      console.error(
+        'error getting user, invalid lookup',
+        lookup,
+        typeof lookup,
+      );
+      throw new Error('error looking up user by text');
     }
-    const { username, email } = criteria;
 
+    const usernameLookup = () => this.getUserByCriteriaOrFail(lookup, null);
+    const emailLookup = () => this.getUserByCriteriaOrFail(null, lookup);
+
+    let attemptFunctions = [usernameLookup, emailLookup];
+    // if it looks like an email, try that first
+    if (lookup.includes('@')) {
+      attemptFunctions = [emailLookup, usernameLookup];
+    }
+
+    for (const attemptFn of attemptFunctions) {
+      try {
+        return await attemptFn();
+      } catch (err) {
+        if (err instanceof EntityNotFoundError) {
+          continue;
+        }
+        throw err;
+      }
+    }
+
+    console.info('user not found', lookup);
+    throw new Error('user not found');
+  }
+
+  private async getUserByCriteriaOrFail(
+    username?: string,
+    email?: string,
+  ): Promise<User> {
     const qb = this.users.createQueryBuilder('user');
-    if (criteria.username) {
+
+    if (username) {
       qb.where('user.username = :username', {
         username: username.toLowerCase(),
       });
-    } else if (criteria.email) {
+    } else if (email) {
       qb.where('user.email = :email', { email: email.toLowerCase() });
     }
 
-    try {
-      return await qb.getOneOrFail();
-    } catch (err) {
-      if (err instanceof EntityNotFoundError) {
-        console.info('lookup for user failed', criteria);
-      }
-      throw err;
-    }
+    return qb.getOneOrFail();
   }
 
   async getPasswordHistory(userId: number): Promise<UserPasswordHistory[]> {
