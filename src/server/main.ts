14ca540178e0d373cbdeb8e7c99e6a82fe8858e7
Brian Kopp <briankopp.usa@gmail.com>
2021-07-02 20:18:35 -0600

    Register validation things

--- src/server/main.ts
+++ src/server/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import * as session from 'express-session';
 import { AppModule } from './app.module';
 import * as passport from 'passport';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   app.use(
     session({
       secret: 'foobar',
