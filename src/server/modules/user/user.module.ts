de0194c61181a49b7063a3867774d22d6bf913a2
Brian Kopp <briankopp.usa@gmail.com>
2021-07-02 21:05:13 -0600

    add user email verification to user module

--- src/server/modules/user/user.module.ts
+++ src/server/modules/user/user.module.ts
@@ -4,10 +4,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/server/entities/user.entity';
 import { UserPasswordHistory } from 'src/server/entities/user-password-history.entity';
 import { UserService } from './services/user.service';
+import { UserEmailsService } from './services/user-emails.service';
+import { UserEmailVerification } from 'src/server/entities/user-email-verification.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, UserPasswordHistory])],
-  providers: [UserDataService, UserService],
+  imports: [
+    TypeOrmModule.forFeature([
+      User,
+      UserPasswordHistory,
+      UserEmailVerification,
+    ]),
+  ],
+  providers: [UserDataService, UserService, UserEmailsService],
   exports: [UserService],
 })
 export class UserModule {}
