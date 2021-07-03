370c38253070dd775996ae7a091743e7acf47c68
Brian Kopp <briankopp.usa@gmail.com>
2021-07-03 16:36:52 -0600

    Start using configuration service

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -10,31 +10,45 @@ import { User } from './entities/user.entity';
 import { UserPasswordHistory } from './entities/user-password-history.entity';
 import { UserEmailVerification } from './entities/user-email-verification.entity';
 import { UserPasswordReset } from './entities/user-password-reset.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'postgres',
-      entities: [User, UserPasswordHistory, UserEmailVerification, UserPasswordReset],
-      synchronize: true,
-    }),
-    MailerModule.forRoot({
-      defaults: {
-        from: '"noreply" <noreply@example.com>',
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: configService.get('DATABASE_HOST', 'localhost'),
+          port: configService.get<number>('DATABASE_PORT', 5432),
+          username: configService.get('DATABASE_USER', 'postgres'),
+          password: configService.get('DATABASE_PASSWORD', 'postgres'),
+          database: configService.get('DATABASE_NAME', 'postgres'),
+          synchronize: configService.get('ENVIRONMENT') === 'local',
+          entities: [User, UserPasswordHistory, UserEmailVerification, UserPasswordReset],
+        };
       },
-      transport: 'smtps://user@domain.com:pass@smtp.domain.com',
-      preview: true,
-      template: {
-        dir: join(__dirname, 'emails'),
-        adapter: new HandlebarsAdapter(),
-        options: {
-          strict: true,
-        },
+    }),
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const fromName = configService.get('EMAIL_FROM_NAME', 'noreply');
+        const fromEmail = configService.get('EMAIL_FROM_ADDRESS');
+        return {
+          defaults: {
+            from: `"${fromName}" <${fromEmail}>`,
+          },
+          preview: configService.get('ENVIRONMENT') === 'local',
+          transport: configService.get('EMAIL_TRANSPORT'),
+          template: {
+            dir: join(__dirname, 'emails'),
+            adapter: new HandlebarsAdapter(),
+            options: {
+              strict: true,
+            },
+          },
+        };
       },
     }),
     UserModule,
