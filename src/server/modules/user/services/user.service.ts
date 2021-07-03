0829f31842fdabdeee1f607dc7f937657a78a2b9
Brian Kopp <briankopp.usa@gmail.com>
2021-07-02 21:05:36 -0600

    Add send email to create user fn

--- src/server/modules/user/services/user.service.ts
+++ src/server/modules/user/services/user.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { User } from 'src/server/entities/user.entity';
 import { UserDataService } from './user-data.service';
+import { UserEmailsService } from './user-emails.service';
 
 @Injectable()
 export class UserService {
-  constructor(private userData: UserDataService) {}
+  constructor(
+    private userData: UserDataService,
+    private emails: UserEmailsService,
+  ) {}
 
   async getUser(lookup: string): Promise<User> {
     return await this.userData.getUser(lookup);
@@ -13,7 +17,13 @@ export class UserService {
   async createUser(user: User): Promise<User> {
     const newUser = await this.userData.insertUser(user);
 
-    // TODO do other things like send email verification
+    const now = new Date();
+    const expirationDate = new Date(now.setMonth(now.getMonth() + 1));
+    const savedVerification = await this.userData.createEmailVerification(
+      user,
+      expirationDate,
+    );
+    await this.emails.sendEmailVerification(savedVerification);
 
     return newUser;
   }
