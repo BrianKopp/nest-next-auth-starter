fbce29593425ece524585290454a5ad0599b5e27
Brian Kopp <briankopp.usa@gmail.com>
2021-07-02 19:14:21 -0600

    Add basic postgres dependencies

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { ViewsModule } from './modules/views/views.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule, UserModule, ViewsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [],
+      synchronize: true,
+    }),
+    AuthModule,
+    UserModule,
+    ViewsModule,
+  ],
   controllers: [],
   providers: [],
 })
